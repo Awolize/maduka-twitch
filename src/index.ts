28dfa0b090d60cd1d3c9f19f2702b63b81f8c895
Alexander Eriksson <alex.store@hotmail.com>
2023-05-07 17:49:28 +0200

    created first test version

---
+++ src/index.ts
@@ -0,0 +1,89 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import { TwitchApi } from 'node-twitch'
+import HttpServer from './server.js'
+import open from 'open'
+import { parseOptions } from './utils.js'
+
+let server: HttpServer
+const PORT = 3000
+const api = new TwitchApi({
+    client_id: process.env.CLIENT_ID!,
+    client_secret: process.env.CLIENT_SECRET!,
+    redirect_uri: 'http://localhost:' + PORT,
+    scopes: ['channel:read:redemptions', 'channel:manage:redemptions'],
+})
+
+server = new HttpServer({ port: PORT })
+
+// --------------------------------------------------------
+
+async function setup() {
+    await new Promise(async (resolve) => {
+        const authUrl = api.generateAuthUrl()
+        await open(authUrl)
+
+        server.on('code', async (code) => {
+            await api.getUserAccess(code)
+            console.log(code)
+            console.log(api.access_token)
+
+            resolve(undefined)
+        })
+    })
+}
+
+// --------------------------------------------------------
+
+const customGet = async (endpoint: string): Promise<any> => {
+    const url = api.base + endpoint
+    const options = {
+        method: 'GET',
+        headers: {
+            'Client-ID': api.client_id,
+            Authorization: `Bearer ${api.access_token}`,
+        },
+    }
+
+    const response = await fetch(url, options)
+    const result = await response.json()
+    return result
+}
+
+// --------------------------------------------------------
+
+export interface GetChannelRewardsOptions {
+    /** User ID of the broadcaster */
+    broadcaster_id: string
+    id?: string
+    only_manageable_rewards?: boolean
+}
+
+/** Gets the channel stream key for a user. */
+const getChannelRewards = async (
+    options: GetChannelRewardsOptions
+): Promise<string> => {
+    const query = '?' + parseOptions(options)
+    const endpoint = `/channel_points/custom_rewards${query}`
+
+    const result = await customGet(endpoint)
+    console.log(result)
+
+    return result.data[0]
+}
+
+// --------------------------------------------------------
+
+server.start()
+
+await setup()
+const result1 = await api.getCurrentUser()
+console.log(result1)
+
+if (result1) {
+    const result2 = await getChannelRewards({ broadcaster_id: result1.id })
+    console.log(result2)
+}
+
+server.stop()
---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+/**
+ * Parse an array into a query string where every value has the same key.
+ * @param {string} key - The key to use. api will be repeated in the query for every value in the array
+ * @param {string[]|string} arr - Array of values to parse into query string.
+ */
+export function parseArrayToQueryString(
+    key: string,
+    arr: readonly unknown[]
+): string {
+    const list = Array.isArray(arr) ? arr : [arr]
+    const result = list.map((value) => `${key}=${value}`).join('&')
+
+    return result
+}
+
+/**
+ * Parses an object into a query string. If the value of a property is an array, that array will be parsed with the `parseArrayToQueryString` function. If a value is undefined or null, it will be skipped.
+ * @param {Object} options - The options to parse.
+ */
+export function parseOptions<T>(options: T): string {
+    let query = ''
+
+    for (const key in options) {
+        const value = options[key]
+
+        if (value === null || value === undefined) continue
+
+        if (Array.isArray(value)) query += parseArrayToQueryString(key, value)
+        else query += `${key}=${value}&`
+    }
+
+    return query.replace(/&$/, '')
+}
